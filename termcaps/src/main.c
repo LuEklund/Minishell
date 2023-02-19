baf4beadacd981153ad62f729c3106a1cbe25c45
Lucas Eklund <luckeeklund@gmail.com>
2023-02-19 10:02:48 +0200

    some tesing

--- termcaps/src/main.c
+++ termcaps/src/main.c
@@ -11,39 +11,81 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
+#include <ctype.h>
 struct termios orig_termios;
+
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
 
 void disableRawMode() {
 	printf("\033[0;100mDISABLE-RAW-MODE\033[0m \n");
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		die("tcetattr");
 }
 
-void	go_raw(void)
+void	enableRawMode(void)
 {
-	struct termios	tc;
+	struct termios	raw;
 
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	 if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+	 	die("tcgetattr");
 	atexit(disableRawMode);
-	tc = orig_termios;
-	tc.c_lflag &= ~(ISIG);
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &tc);
+	raw = orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 4;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+		die("tcsetattr");
 }
 
-int main(void)
-{
-	char	*buf;
 
-	go_raw();
-	buf = NULL;	
-	while (ft_strncmp(buf, "exit", 5))
-	{
-		if (buf)
-			free(buf);
-		buf = readline("\033[0;32mDinoshell>\033[0m ");
-	}
-	if (buf)
-		free(buf);
+int main() {
+	char c = '\0';
+
+	// char	*buf;
+
+
+	enableRawMode();
+	// buf = NULL;	
+	// buf = readline("\033[0;32mDinoshell>\033[0m ");
+	while (1) {
+	c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1)
+		die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
+	// if (buf)
+		// free(buf);
 	printf("\033[0;95mexit\033[0m 🦕\n");
-	return (0);
+	return 0;
 }
+
+// int main(void)
+// {
+// 	char	*buf;
+
+// 	go_raw();
+// 	buf = NULL;	
+// 	while (ft_strncmp(buf, "exit", 5))
+// 	{
+// 		if (buf)
+// 			free(buf);
+// 		buf = readline("\033[0;32mDinoshell>\033[0m ");
+// 	}
+// 	if (buf)
+// 		free(buf);
+// 	printf("\033[0;95mexit\033[0m 🦕\n");
+// 	return (0);
+// }
